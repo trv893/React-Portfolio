fc798d30f927a8b398ac520a81030e725d1fbda5
trv893 <trevorhilimire@gmail.com>
2022-09-07 13:12:13 -0400

    update portfolio

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/trevor_hilimire_resume2.pdf";
+import pdf from "../../Assets/trevor_hilimire_resume3.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/trv893/react-portfolio/release/src/Assets/trevor_hilimire_resume2.pdf";
+  "https://raw.githubusercontent.com/trv893/react-portfolio/release/src/Assets/trevor_hilimire_resume3.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
